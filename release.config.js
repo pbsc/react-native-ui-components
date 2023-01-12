c3d225c0966638091ca47353fc1ba7e16d2d6137
Yousra Saidani <ysaidani@pbsc.com>
2023-01-11 21:35:36 -0500

    :green_heart: ci: add header regex pattern for the commit subject

---
+++ release.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          {
+            type: 'docs',
+            release: 'patch',
+          },
+          {
+            type: 'refactor',
+            release: 'patch',
+          },
+          {
+            type: 'style',
+            release: 'patch',
+          },
+          {
+            type: 'feat',
+            release: 'minor',
+          },
+          {
+            type: 'fix',
+            release: 'patch',
+          },
+          {
+            type: 'perf',
+            release: 'patch',
+          },
+          {
+            type: 'build',
+            release: 'patch',
+          },
+          {
+            type: 'ci',
+            release: 'patch',
+          },
+          {
+            type: 'revert',
+            release: 'patch',
+          },
+        ],
+        parserOpts: {
+          headerPattern: /^(:\w:)?(\w*)(?:\(([\w\$\.\-\* ]*)\))?\: (.*)$/,
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+        },
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github',
+  ],
+};
