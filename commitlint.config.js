851b86f0b3224cc395576bbb27066a35eeea3671
Yousra Saidani <ysaidani@pbsc.com>
2023-01-13 15:36:08 -0500

    major: add new commit type for breaking change
    add major as new commit type for breaking change commits so that major version gets updated
    
    BREAKING CHANGE: this is a test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'major',
+        'ci',
+        'build',
+        'chore',
+        'perf',
+        'test',
+        'revert',
+        'refactor',
+      ],
+    ],
+  },
+};
